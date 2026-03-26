2b265b6e61fd46a3249b6c045c7605ec9e2a110f
fifassss <133858543+fifassss@users.noreply.github.com>
2026-03-27 06:50:07 +0700

    Add files via upload

---
+++ ClientWebtest.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <thread>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#pragma warning(disable: 4996)
+
+class Client_chats
+{
+private:
+    SOCKET client;
+    SOCKADDR_IN serveraddr;
+
+
+
+public:
+    Client_chats(const std::string& serverip, int port)
+    {
+        WSADATA wsaData;
+        WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+        client = socket(AF_INET, SOCK_STREAM, 0);
+        serveraddr.sin_family = AF_INET;
+        serveraddr.sin_port = htons(port);
+        inet_pton(AF_INET, serverip.c_str(), &serveraddr.sin_addr);
+    }
+    ~Client_chats()
+    {
+        closesocket(client);
+        WSACleanup();
+    }
+    void Connect()
+    {
+        if (connect(client, (SOCKADDR*)&serveraddr, sizeof(serveraddr)) < 0)
+        {
+            std::cerr << "Connection failed." << '\n';
+            exit(EXIT_FAILURE);
+        }
+
+        std::cout << "Connected to the server. Welcome to the chat!" << '\n';
+    }
+    void send_message(const std::string& message)
+    {
+        send(client, message.c_str(), message.length(), 0);
+    }
+    void MessageReceive()
+    {
+        char buffer[1024];
+        while (true)
+        {
+            int bytesreceived = recv(client, buffer, sizeof(buffer),0);
+            if (bytesreceived == SOCKET_ERROR || bytesreceived == 0)
+            {
+                std::cout << "Disconnet from server." << '\n';
+                break;
+            }
+            std::cout << std::string(buffer, bytesreceived) << '\n';
+        }
+    }
+    void runs()
+    {
+        std::thread receivethread([this] {MessageReceive(); });
+        receivethread.detach();
+
+        std::string input{};
+        std::string username{};
+        
+
+        std::cout << "Enter your username: \n";
+        std::getline(std::cin, username);
+        send_message(username);
+
+
+        int client_num{ 0 };
+        int bytes_received = recv(client, (char*)&client_num, sizeof(client_num), 0);
+        int num = ntohl(client_num); // обратное преобразование
+
+        std::cout << num << '\n';
+
+
+
+        while (true)
+        {
+            std::getline(std::cin, input);
+            send_message(input);
+            if (input == "exit")
+            {
+                break;
+            }
+        }
+    }
+};
+
+int main() 
+{
+    std::string server_ip{};
+    std::cout << "Enter server ip: ";
+    std::cin >> server_ip;
+    Client_chats client(server_ip, 1500);
+    client.Connect();
+    client.runs();
+    
+    return 0;
+}
---
+++ Webtest.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <thread>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+
+#pragma comment(lib, "ws2_32.lib")
+
+#pragma warning(disable: 4996)
+
+
+
+class ChatServer
+{
+private:
+    SOCKET serverSocket;
+    std::unordered_map<std::string, SOCKET> clients;
+    int indexid; // уникальный айди для каждого из пользователей(НАХОДИТСЯ В ДОРАБОТКЕ)
+public:
+    ChatServer(int Port) : indexid(0) // здесь мы создали сокет сервера.
+    {
+        WSAData wsaData;
+        WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+
+        serverSocket = socket(AF_INET, SOCK_STREAM,0);
+        SOCKADDR_IN addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(Port);
+        addr.sin_addr.s_addr = INADDR_ANY;
+
+
+        bind(serverSocket, (SOCKADDR*)&addr, sizeof(addr));
+
+        listen(serverSocket, 4);
+    }
+    ~ChatServer()
+    {
+        closesocket(serverSocket);
+        WSACleanup();
+    }
+
+    void AcceptClient()
+    {
+        while (true)
+        {
+            SOCKET clientsocket;
+            SOCKADDR_IN addr_client;
+            int lenaddr_client = sizeof(addr_client);
+            clientsocket = accept(serverSocket, (SOCKADDR*)&addr_client, &lenaddr_client);
+
+            std::thread Clientthread([this, clientsocket] {handlerclient(clientsocket); });
+            Clientthread.detach();
+        }
+    }
+    void handlerclient(SOCKET client)
+    {
+        int net_num = htonl(indexid++); // преобразование что бы отправить
+        send(client, (char*)&net_num, sizeof(net_num), 0);
+        
+        char buffer[1024];
+        int bytesreceived = recv(client, buffer, sizeof(buffer), 0);
+        std::string clietname(buffer, bytesreceived);
+        clients[clietname] = client;
+
+
+        std::cout << "Client connected: " << clietname << " " << " Id: " << indexid << '\n';
+
+        while (true)
+        {
+            bytesreceived = recv(client, buffer, sizeof(buffer), 0);
+            if (bytesreceived <= 0)
+            {
+                std::cout << "Client disconnected: " << clietname << " " << " Id: " << indexid << '\n';
+                clients.erase(clietname);
+                closesocket(client);
+                break;
+            }
+            std::string messageReceived(buffer, bytesreceived);
+            std::cout << "Received from " << clietname << ": " << messageReceived << '\n';
+
+            std::string buffer_to_send = clietname + ": " + messageReceived;
+            for (auto& Pair : clients)
+            {
+                if (Pair.first != clietname)
+                { // что бы не отправлять самому отправителю сообщение
+                    send(Pair.second, buffer_to_send.c_str(), buffer_to_send.length(),0);
+                }
+            }
+        }
+    }
+};
+
+
+
+
+int main() 
+{
+    ChatServer server(1500);
+
+    server.AcceptClient();
+    return 0;
+}
